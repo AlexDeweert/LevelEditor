13363b14ff02a9d76c6c447085caecd0ebe745db
AlexDeweert <alexander.deweert@gmail.com>
2018-06-12 09:51:45 -0700

    Clickable grid with correct columns and ability to populate grid cell with an image

--- GridWindow.xaml.cs
+++ GridWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,7 @@ namespace LevelEditor
         public GridWindow()
         {
             InitializeComponent();
-            CreateGrid(5,5,dynamicGrid, 70);
+            CreateGrid(25,25,dynamicGrid, 32);
         }
 
         /*CreateGrid
@@ -33,6 +34,44 @@ namespace LevelEditor
          */
         public void CreateGrid( int cols, int rows, Grid parentGrid, int tilesize )
         {
+            MouseButtonEventHandler leftclick = new MouseButtonEventHandler((e, args) => {
+                if ( args.ClickCount == 1)
+                {
+                    var point = args.GetPosition(parentGrid);
+                    //The variable row/col at the top/left that keeps things centered
+                    var paddingRowHeight = parentGrid.RowDefinitions[0].ActualHeight;
+                    var paddingColWidth = parentGrid.ColumnDefinitions[0].ActualWidth;
+                    int rowclicked = (int)(point.Y - paddingRowHeight) / tilesize;
+                    int colclicked = (int)(point.X - paddingColWidth) / tilesize;
+                    if (!(rowclicked > rows || rowclicked < 0 || colclicked > cols || colclicked < 0))
+                    {
+                        System.Diagnostics.Debug.WriteLine("Detected a LEFT mouse click => X: " + point.X + ", Y: " + point.Y);
+                        System.Diagnostics.Debug.WriteLine("Cell was => ROW: " + rowclicked + ", COL: " + colclicked);
+                        System.Diagnostics.Debug.WriteLine("Padding was => ROW: " + paddingRowHeight + ", COL: " + paddingColWidth);
+                        System.Diagnostics.Debug.WriteLine(" ");
+                        Image image = new Image();
+                        image.Height = tilesize;
+                        image.Width = tilesize;
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        //URI: "Uniform Resource Identifiers"
+                        bitmapImage.UriSource = new Uri("/Images/grassMid.png", UriKind.Relative);
+                        bitmapImage.DecodePixelHeight = tilesize;
+                        bitmapImage.DecodePixelWidth = tilesize;
+                        bitmapImage.EndInit();
+                        image.Source = bitmapImage;
+                        Grid.SetColumn(image, colclicked+1);
+                        Grid.SetRow(image, rowclicked+1);
+                        parentGrid.Children.Add(image);
+                    }
+                    
+                }
+            });
+
+            parentGrid.PreviewMouseLeftButtonDown += leftclick;
+            parentGrid.Background = new SolidColorBrush(Colors.Transparent);
+
+            /*UNCOMMENT these if you would like the grid to be centered*/
             ColumnDefinition c0 = new ColumnDefinition();
             c0.Width = new GridLength(1, GridUnitType.Star);
             parentGrid.ColumnDefinitions.Add(c0);
@@ -59,20 +98,20 @@ namespace LevelEditor
             r2.Height = new GridLength(1, GridUnitType.Star);
             parentGrid.RowDefinitions.Add(r2);
 
-            Image image = new Image();
-            image.Height = tilesize;
-            image.Width = tilesize;
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            bitmapImage.UriSource = new Uri(@"/LevelEditor;/Images/grass.png", UriKind.Relative);
-            bitmapImage.DecodePixelHeight = tilesize;
-            bitmapImage.DecodePixelWidth = tilesize;
-            bitmapImage.EndInit();
-            image.Source = bitmapImage;
-
-            Grid.SetColumn(image, 1);
-            Grid.SetRow(image, 1);
-            parentGrid.Children.Add(image);
+            //Image image = new Image();
+            //image.Height = tilesize;
+            //image.Width = tilesize;
+            //BitmapImage bitmapImage = new BitmapImage();
+            //bitmapImage.BeginInit();
+            ////URI: "Uniform Resource Identifiers"
+            //bitmapImage.UriSource = new Uri("/Images/grassMid.png", UriKind.Relative);
+            //bitmapImage.DecodePixelHeight = tilesize;
+            //bitmapImage.DecodePixelWidth = tilesize;
+            //bitmapImage.EndInit();
+            //image.Source = bitmapImage;
+            //Grid.SetColumn(image, 1);
+            //Grid.SetRow(image, 1);
+            //parentGrid.Children.Add(image);
 
 
             /*POPULATE ALL CELLS (with a button in this case)*/
