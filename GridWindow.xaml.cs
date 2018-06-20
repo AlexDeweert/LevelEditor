53b60e85879c2970cf1e83f38bafec9017ea6743
AlexDeweert <alexander.deweert@gmail.com>
2018-06-20 09:18:59 -0700

    daily commit

--- GridWindow.xaml.cs
+++ GridWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,10 +23,11 @@ namespace LevelEditor
     /// </summary>
     public partial class GridWindow : Window
     {
+
         public GridWindow()
         {
             InitializeComponent();
-            CreateGrid(25,25,dynamicGrid, 32);
+            CreateGrid(5,5,dynamicGrid, 32);
         }
 
         /*CreateGrid
@@ -35,41 +37,11 @@ namespace LevelEditor
         public void CreateGrid( int cols, int rows, Grid parentGrid, int tilesize )
         {
             MouseButtonEventHandler leftclick = new MouseButtonEventHandler((e, args) => {
-                if ( args.ClickCount == 1)
-                {
-                    var point = args.GetPosition(parentGrid);
-                    //The variable row/col at the top/left that keeps things centered
-                    var paddingRowHeight = parentGrid.RowDefinitions[0].ActualHeight;
-                    var paddingColWidth = parentGrid.ColumnDefinitions[0].ActualWidth;
-                    int rowclicked = (int)(point.Y - paddingRowHeight) / tilesize;
-                    int colclicked = (int)(point.X - paddingColWidth) / tilesize;
-                    if (!(rowclicked > rows || rowclicked < 0 || colclicked > cols || colclicked < 0))
-                    {
-                        System.Diagnostics.Debug.WriteLine("Detected a LEFT mouse click => X: " + point.X + ", Y: " + point.Y);
-                        System.Diagnostics.Debug.WriteLine("Cell was => ROW: " + rowclicked + ", COL: " + colclicked);
-                        System.Diagnostics.Debug.WriteLine("Padding was => ROW: " + paddingRowHeight + ", COL: " + paddingColWidth);
-                        System.Diagnostics.Debug.WriteLine(" ");
-                        Image image = new Image();
-                        image.Height = tilesize;
-                        image.Width = tilesize;
-                        BitmapImage bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
-                        //URI: "Uniform Resource Identifiers"
-                        bitmapImage.UriSource = new Uri("/Images/grassMid.png", UriKind.Relative);
-                        bitmapImage.DecodePixelHeight = tilesize;
-                        bitmapImage.DecodePixelWidth = tilesize;
-                        bitmapImage.EndInit();
-                        image.Source = bitmapImage;
-                        Grid.SetColumn(image, colclicked+1);
-                        Grid.SetRow(image, rowclicked+1);
-                        parentGrid.Children.Add(image);
-                    }
-                    
-                }
+                HandleLeftClickOnGrid(parentGrid, args, tilesize, rows, cols);
             });
 
             parentGrid.PreviewMouseLeftButtonDown += leftclick;
-            parentGrid.Background = new SolidColorBrush(Colors.Transparent);
+            //parentGrid.Background = new SolidColorBrush(Colors.Transparent);
 
             /*UNCOMMENT these if you would like the grid to be centered*/
             ColumnDefinition c0 = new ColumnDefinition();
@@ -112,24 +84,134 @@ namespace LevelEditor
             //Grid.SetColumn(image, 1);
             //Grid.SetRow(image, 1);
             //parentGrid.Children.Add(image);
+            
+        BrushConverter bc = new BrushConverter();
+            /*POPULATE ALL CELLS with a rect*/
+            for (int i = 1; i <= cols; i++)
+            {
+                for (int j = 1; j <= rows; j++)
+                {
+                    //Set the grid space cell indicators in a checkerboard-like pattern
+                    if (i % 2 == 0 && j % 2 == 0 )
+                    {
+                        Rectangle newRect = GetRectangle(0XFF, 0xF8, 0xF8, 0xF8);
+                        Grid.SetColumn(newRect, i); Grid.SetRow(newRect, j); parentGrid.Children.Add(newRect);
+                    }
+                    else if (i % 2 == 1 && j % 2 == 0)
+                    {
+                        Rectangle newRect = GetRectangle(0X77, 0xA9, 0xA9, 0xA9);
+                        Grid.SetColumn(newRect, i); Grid.SetRow(newRect, j); parentGrid.Children.Add(newRect);
+                    }
+                    else if (i % 2 == 1 && j % 2 == 1)
+                    {
+                        Rectangle newRect = GetRectangle(0XFF, 0xF8, 0xF8, 0xF8);
+                        Grid.SetColumn(newRect, i); Grid.SetRow(newRect, j); parentGrid.Children.Add(newRect);
+                    }
+                    else if (i % 2 == 0 && j % 2 == 1)
+                    {
+                        Rectangle newRect = GetRectangle(0X77, 0xA9, 0xA9, 0xA9);
+                        Grid.SetColumn(newRect, i); Grid.SetRow(newRect, j); parentGrid.Children.Add(newRect);
+                    }
+                }
+            }
+        }
 
+        public Rectangle GetRectangle(byte transparency, byte red, byte green, byte blue)
+        {
+            return new Rectangle()
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Fill = new SolidColorBrush(Color.FromArgb(transparency, red, green, blue))
+            };
+        }
 
-            /*POPULATE ALL CELLS (with a button in this case)*/
-            //for (int i = 1; i <= cols; i++)
-            //{
-            //    for (int j = 1; j <= rows; j++)
-            //    {
-            //        Button b1 = new Button
-            //        {
-            //            HorizontalAlignment = HorizontalAlignment.Stretch,
-            //            VerticalAlignment = VerticalAlignment.Stretch,
-            //            Content = ("b" + i.ToString() + " " + j.ToString())
-            //        };
-            //        Grid.SetColumn(b1, i);
-            //        Grid.SetRow(b1, j);
-            //        parentGrid.Children.Add(b1);
-            //    }
-            //}
+        public void LoadAllTextures()
+        {
+            ResourceDictionary resourceDictionary = new ResourceDictionary();
+            resourceDictionary.Source = new Uri("/Images/", UriKind.Relative);
+        }
+
+        public void HandleLeftClickOnGrid(Grid parentGrid, MouseButtonEventArgs args, int tilesize, int rows, int cols)
+        {
+            if (args.ClickCount == 1)
+            {
+                var point = args.GetPosition(parentGrid);
+                //The variable row/col at the top/left that keeps things centered
+                var paddingRowHeight = parentGrid.RowDefinitions[0].ActualHeight;
+                var paddingColWidth = parentGrid.ColumnDefinitions[0].ActualWidth;
+                int rowclicked = (int)(point.Y - paddingRowHeight) / tilesize;
+                int colclicked = (int)(point.X - paddingColWidth) / tilesize;
+                if (!(rowclicked > rows || rowclicked < 0 || colclicked > cols || colclicked < 0))
+                {
+                    System.Diagnostics.Debug.WriteLine("Detected a LEFT mouse click => X: " + point.X + ", Y: " + point.Y);
+                    System.Diagnostics.Debug.WriteLine("Cell was => ROW: " + rowclicked + ", COL: " + colclicked);
+                    System.Diagnostics.Debug.WriteLine("Padding was => ROW: " + paddingRowHeight + ", COL: " + paddingColWidth);
+                    System.Diagnostics.Debug.WriteLine(" ");
+                    Image image = new Image();
+                    image.Height = tilesize;
+                    image.Width = tilesize;
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    //URI: "Uniform Resource Identifiers"
+                    bitmapImage.UriSource = new Uri("/Images/grassMid.png", UriKind.Relative);
+                    bitmapImage.DecodePixelHeight = tilesize;
+                    bitmapImage.DecodePixelWidth = tilesize;
+                    bitmapImage.EndInit();
+                    image.Source = bitmapImage;
+                    Grid.SetColumn(image, colclicked + 1);
+                    Grid.SetRow(image, rowclicked + 1);
+                    parentGrid.Children.Add(image);
+                }
+
+            }
+        }
+
+        private void ImportTexture(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("IMPORT TEXTURE clicked");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            //openFileDialog.InitialDirectory = @"c:\";
+            /*Uses the dictory last used*/
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                CopyFileToProjectDirectory(openFileDialog);
+            }
+
+            //TODO After importing the texture, making the resource available to the program thereafter
+            //ResourceManager?
+        }
+
+        private void CopyFileToProjectDirectory(OpenFileDialog dialog)
+        {
+            try
+            {
+                //Opens the dialog for the user to select and stores the result as a variable
+                var dialogFilename = dialog.FileName;
+
+                //Get the filename
+                string filename = System.IO.Path.GetFileName(dialogFilename);
+                System.Diagnostics.Debug.WriteLine("Got import filename of: " + filename);
+
+                //Get the full file path only
+                string filePathOnly = System.IO.Path.GetDirectoryName(dialogFilename);
+                System.Diagnostics.Debug.WriteLine("Got import filePathOnly of: " + filePathOnly);
+
+                //Get the destination directory path
+                string thisProgramDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                string copyToFolder = "\\images\\";
+                thisProgramDirectory += copyToFolder;
+                System.Diagnostics.Debug.WriteLine("Got this program dir is: " + thisProgramDirectory);
+
+                //Finally copy the file
+                File.Copy(System.IO.Path.Combine(filePathOnly, filename), System.IO.Path.Combine(thisProgramDirectory, filename), true);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("Error copying texture to destination: " + e.ToString());
+            }
         }
     }
 }
